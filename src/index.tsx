f596cd2b4e87082405f640a510a033da43be9eba
Joseph Podwys <joseph@podwys.com>
2021-05-27 11:30:20 -0600

    Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import { blur } from './core/vanilla/blur';
 import setupVideo from './core/vanilla/track';
 
-async function setup() {
+(async () => {
   const video = document.getElementById('input') as HTMLVideoElement;
   const canvas = document.getElementById('output') as HTMLCanvasElement;
   await setupVideo(video, canvas);
   await blur(video, canvas);
-}
-
-setup()
+})();
